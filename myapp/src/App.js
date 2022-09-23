a07f5f52e111caacd258cee1e8cf8320e6b0a798
zircod <zircod@mail.ru>
2022-09-23 18:48:42 +0300

    hm #2 with error

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  const message = 'Hi, I am leaning React'
+  const [messagesList, setMessagesList] = useState([
+    {
+      text: '',
+      author: ''
+    },
+    {
+      text: '',
+      author: ''
+    }
+  ]);
+  const [messagesBody, setMessagesBody] = useState({
+    test: '',
+    author: ''
+  });
+
+  const ROBOT_MESSAGE = 'Привет! Я получил твое сообщение.';
+
+  useEffect(() => {
+    if (messagesList.length > 0 && messagesList.slice(-1)[0].author !== 'robot') {
+      setTimeout(() => {
+        setMessagesList(pervstate => [...pervstate, {text:ROBOT_MESSAGE, author:'robot'}])
+      }, 1500)
+    }
+  }, [messagesList])
+
+
 
   return (
     <div className="App">
-      <Myheader message = {message} />
+        <Form data={messagesBody} setData={setMessagesBody} setMessages={setMessagesList}/>
+        <div className="messagesList">
+          {messagesList.map((e) => <Message text={e.text} autor={e.autor} />)}
+        </div>
     </div>
   );
 }
 
 export default App;
 
-const Myheader = ({message}) => {
+const Form = ({ data, setData, setMessage }) => {
 
-  console.log(message)
+      const {text, author} = data;
+
+      const submitForm =(e) => {
+        e.preventDefault();
+        if (text.length > 0) {
+        setMessage(pervstate => [...pervstate, {text, author}])
+        }
+
+        setData(
+          {
+            text: '',
+            author: ''
+          }
+        )
+    }
+
 
   return (
-    <div className='header'>
-      <h1>{message}</h1>
-    </div>
-  );
+    <form onSubmit={submitForm}>
+      <input placeholder='Имя' value={text} onChange = {(e) =>
+        setData(pervstate => ({...pervstate, text: e.target.value})
+      )}/>
+
+      <input placeholder='Текст' value={author} onChange = {(e) =>
+      setData(pervstate => ({...pervstate, author: e.target.value})
+      )}/>
+
+      <button type="submit">Отправить</button>
+    </form>
+  )
 }
